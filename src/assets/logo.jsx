bfbf826eeafea82e32522d205da82b0bae13eb94
Murat <atzauka@gmail.com>
2020-02-12 18:19:07 +0300

    add formik

---
+++ src/assets/logo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="230"
+      height="60"
+      fill="none"
+      viewBox="0 0 230 60"
+    >
+      <circle cx="30" cy="30" r="30" fill="#C4C4C4"></circle>
+      <circle cx="140" cy="30" r="30" fill="#C4C4C4"></circle>
+      <path fill="#C4C4C4" d="M70 0H100V60H70z"></path>
+      <path fill="#C4C4C4" d="M200 0h30l-30 60h-30l30-60z"></path>
+    </svg>
+  );
+}
+
+export default Icon;
